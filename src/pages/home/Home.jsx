78e58ed107706e0a6aed048311092cfaf4d06711
Lagger2ker <majmanh2000@gmail.com>
2023-09-06 17:55:00 +0700

    2commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
 import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/sidebar";
 
 export default function Home() {
   return (
+    <>
     <Topbar/>
-  )
+    <Sidebar/>
+    </>
+  );
 }
